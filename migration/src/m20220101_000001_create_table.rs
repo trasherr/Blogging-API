8af877f4ed676b6803dc1a25a17ea8f3f0f9d7b5
Prakhar Singh <2000prakharsingh@gmail.com>
2023-08-28 21:41:25 +0530

    Second

---
+++ migration/src/m20220101_000001_create_table.rs
@@ -0,0 +1,48 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(User::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(User::Id)
+                            .integer()
+                            .not_null()
+                            .auto_increment()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(User::Name).string().not_null())
+                    .col(ColumnDef::new(User::Email).string().unique_key().not_null())
+                    .col(ColumnDef::new(User::Password).string().not_null())
+                    .col(ColumnDef::new(User::Uuid).uuid().unique_key().not_null())
+                    .col(ColumnDef::new(User::CreatedAt).date_time().not_null())
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+
+        manager
+            .drop_table(Table::drop().table(User::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+enum User {
+    Table,
+    Id,
+    Name,
+    Email,
+    Password,
+    Uuid,
+    CreatedAt
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use axum::{Router, routing::get, response::IntoResponse, http::StatusCode};
-
+use chrono::Utc;
+use sea_orm::{Database, DatabaseConnection, Set, ActiveModelTrait};
+use entity::user;
+use uuid::Uuid;
 
 #[tokio::main]
 async fn main() {
@@ -9,8 +12,10 @@ async fn main() {
 
 async fn server(){
 
+
     let app: Router = Router::new()
-    .route("/api/test",get(test));
+    .route("/api/test",get(test))
+    .route("/api/test/insert",get(create_user));
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
     .serve(app.into_make_service())
@@ -22,4 +27,22 @@ async fn test() -> impl IntoResponse{
     println!("Test Api");
 
     (StatusCode::ACCEPTED,"Hey There")
+}
+
+async fn create_user() -> impl IntoResponse {
+    let db: DatabaseConnection = Database::connect("postgres://trasherr:trasherr@localhost:5432/BlogDB").await.unwrap();
+
+    let user_model = user::ActiveModel{
+      name: Set("test".to_owned()),
+      email: Set("test@gmail.com".to_owned()),
+      password: Set("12345678".to_owned()),
+      uuid: Set(Uuid::new_v4()),
+      created_at: Set(Utc::now().naive_utc()) ,
+        ..Default::default()
+    };
+    let usr = user_model.insert(&db).await.unwrap();
+
+
+    (StatusCode::ACCEPTED,"Inserted")
+
 }
